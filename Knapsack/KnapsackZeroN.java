0e206df246551efc194f925df3b98cc7f6565e8d
Angad Singh Rajput <getangad@gmail.com>
2024-12-22 11:18:30 +0000

    working on 0n knapsack

---
+++ Knapsack/KnapsackZeroN.java
@@ -0,0 +1,55 @@
+/**
+ * 0-1 Knapsack Problem
+ * Wts = [4,3,46,7]
+ * Profit = [1,2,3,7]
+ * 
+ * Knapsack weight = 6
+ * 
+ */
+
+/*
+ * 
+ * if weight is more than k then dont include.
+ * 
+ * 
+ */
+
+class KnapsackZeroN {
+    public int usingRecursion(int[] weight,
+            int[] profit,
+            int knapsackWeight) {
+                if (knapsackWeight == 0) {
+                    return 0;
+                }
+
+                return usingRecursion(weight, profit, knapsackWeight, weight.length - 1);
+    }
+
+    private int usingRecursion(int[] weight,
+            int[] profit,
+            int knapsackWeight,
+            int itemIndex) {
+                if (knapsackWeight <= 0) {
+                    return 0;
+                }
+                int includeValue = 0;
+                //include value
+                if (knapsackWeight >= weight[itemIndex])
+                    includeValue = profit[itemIndex] + usingRecursion(weight, profit, knapsackWeight-weight[itemIndex], itemIndex);
+                //exclude
+                int excludeValue = usingRecursion(weight, profit, knapsackWeight, itemIndex - 1);
+
+                return Math.max(includeValue, excludeValue);
+            }
+
+    public static int usingDynamicProgramming(int[] weight,
+            int[] profit,
+            int knapsackWeight) {
+
+
+                return 0;
+    }
+
+
+   
+}
---
+++ Knapsack/KnapsackZeroNTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class KnapsackZeroNTest {
+
+    @Test()
+    public void testKnapsack() {
+        int[] weight = new int[] {3,4,47,7};
+        int[] profit = new int[] {10,2,3,7};
+        int knapsackWeight  = 81;
+        KnapsackZeroN knapsackZeroN = new KnapsackZeroN();
+        int resultUsingRecursion = knapsackZeroN.usingRecursion(weight, profit, knapsackWeight);
+        Assert.assertEquals(12, resultUsingRecursion);
+    }   
+}
